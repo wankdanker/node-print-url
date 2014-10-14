1d6759d24355d98949efd4621f4527831e63ec02
Dan VerWeire <dverweire@gmail.com>
2014-10-13 22:14:37 -0400

    Accept a string URL

--- index.js
+++ index.js
@@ -5,19 +5,24 @@ var Printer = require('lp-client')
 
 module.exports = print;
 
-
 function print(options, cb) {
 	if (typeof options === 'function') {
 		cb = options;
 		options = null;
 	}
 
+	if (typeof options === 'string') {
+		options = {
+			url : options
+		};
+	}
+
 	if (!options) {
-		return cb(new Error('options object is required'));
+		return cb && cb(new Error('url string or options object is required'));
 	}
 
 	if (!options.url) {
-		return cb(new Eror('options.url is required'));
+		return cb && cb(new Eror('options.url is required'));
 	}
 
 	request(options.url, function (err, response, body) {
